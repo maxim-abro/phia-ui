912142354f2e27d16b82690f1ae13a4db6dadde1
maxim.abro <maxim.abro@yandex.ru>
2023-09-26 22:03:42 +0300

    feat: todo dropdown

---
+++ src/components/navigation/MDropdown.vue
@@ -0,0 +1,32 @@
+<template>
+  <div v-if="type === 'text'" class="text-primary">
+    <slot />
+  </div>
+  <m-button v-if="type === 'button'">
+    <slot />
+  </m-button>
+
+  <div v-if="isOpen" class="">
+    <slot name="dropdown"/>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { DropdownType } from '@/types/DropdownType'
+import MButton from '@/App.vue'
+import { Ref, ref, UnwrapRef } from "vue";
+
+const props = withDefaults(defineProps<DropdownType>(), {
+  type: 'text',
+  maxHeight: 50,
+  disabled: false,
+  trigger: 'hover',
+  hideTimeout: 250
+})
+
+const isOpen:Ref<UnwrapRef<boolean>> = ref(false)
+
+function openMenu() {
+  isOpen.value = false
+}
+</script>
---
+++ src/types/DropdownType.ts
@@ -0,0 +1,8 @@
+
+export interface DropdownType {
+  type?: 'button' | 'text'
+  maxHeight?: number
+  disabled?: boolean
+  trigger?: 'hover' | 'click'
+  hideTimeout?: number
+}
